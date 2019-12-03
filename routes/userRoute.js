6831975bda9e6a99dc68eb8573e5e73931887081
Victor Albani <vdalbani-rodriguez@myseneca.ca>
2019-12-03 17:09:14 -0500

    adding_loginFunction2

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -32,70 +32,82 @@ router.post("/registration",(req,res)=>
     };
     
 
-    const reg_errors=[]; 
+    const reg_errors=[];
 
-    if(reg_formData.new_userEmail == '')
+    newUsers.findOne({new_userEmail:reg_formData.new_userEmail})
+    .then(thisUser=>
     {
-        reg_errors.push('Please enter email')
-       
-    }
+        if(thisUser != null){
+            reg_errors.push("USER ALREADY EXIST");
+            res.render("userViews/registration",{reg_message:reg_errors});
+            console.log(thisUser);
+        }
+        else
+        {
+            if(reg_formData.new_userEmail == '')
+            {
+                reg_errors.push('Please enter email')
+            
+            }
 
-    if(reg_formData.new_userName=='')
-    {
-        reg_errors.push('Please enter first name')
-    }
+            if(reg_formData.new_userName=='')
+            {
+                reg_errors.push('Please enter first name')
+            }
 
-    if(reg_formData.new_userLast=='')
-    {
-        reg_errors.push('Please enter last name')
-    }
+            if(reg_formData.new_userLast=='')
+            {
+                reg_errors.push('Please enter last name')
+            }
 
-    if(reg_formData.new_userPass=='')
-    {
-        reg_errors.push('Please enter a password')
-    }
+            if(reg_formData.new_userPass=='')
+            {
+                reg_errors.push('Please enter a password')
+            }
 
 
-    if(reg_formData.new_userBDay=='')
-    {
-        reg_errors.push('Please enter day')
-    }
+            if(reg_formData.new_userBDay=='')
+            {
+                reg_errors.push('Please enter day')
+            }
 
-    if(reg_formData.new_userBMonth=='')
-    {
-        reg_errors.push('Please enter month')
-    }
+            if(reg_formData.new_userBMonth=='')
+            {
+                reg_errors.push('Please enter month')
+            }
 
-    if(reg_formData.new_userBYear=='')
-    {
-        reg_errors.push('Please enter year')
-    }
+            if(reg_formData.new_userBYear=='')
+            {
+                reg_errors.push('Please enter year')
+            }
 
-    //ERROR VALIDATION
-    if(reg_errors.length > 0)
-    {
-         res.render("userViews/registration",{reg_message:reg_errors});
-    
-    }else{
-      //  res.render("userViews/registration");
-        console.log("INFO PASSED SUCCESFULLY");
-        console.log(reg_formData);
-        
-        //res.redirect("/");
-       
-        const pbNewUser= new newUsers(reg_formData);
+            //ERROR VALIDATION
+            if(reg_errors.length > 0)
+            {
+                res.render("userViews/registration",{reg_message:reg_errors});
+            
+            }else{
+            //  res.render("userViews/registration");
+                console.log("INFO PASSED SUCCESFULLY");
+                console.log(reg_formData);
+                
+                //res.redirect("/");
+            
+                const pbNewUser= new newUsers(reg_formData);
 
-        pbNewUser.save().then(()=>{console.log(`user inserted`)})
-        .catch((err)=>{console.log(`Wrong because ${err}`)});
+                pbNewUser.save().then(()=>{console.log(`user inserted`)})
+                .catch((err)=>{console.log(`Wrong because ${err}`)});
 
-        //res.render('other/home',{w_username:pbNewUser});
-        //console.log(`USER ID =${pbNewUser} email: ${reg_formData.new_userEmail} name: ${reg_formData.new_userName} saved`);
+                //res.render('other/home',{w_username:pbNewUser});
+                //console.log(`USER ID =${pbNewUser} email: ${reg_formData.new_userEmail} name: ${reg_formData.new_userName} saved`);
 
-        //if sessions it should be here
-        res.render("homeViews/home");
-    }
+                //if sessions it should be here
+                res.render("homeViews/home");
+            }
+        }
+            
+    });
 });
-
 /********LOG IN ************* */
 //GETTING THE LOG IN VIEW ROUTE
 router.get('/log_in',(req,res)=>
